17d01e5848fde843a9eefe37056c2e639899d8ad
djaler <djaler1@gmail.com>
2023-12-01 23:14:31 +0300

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,39 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val firstNumber = line.first { it.isDigit() }.digitToInt()
+            val lastNumber = line.last { it.isDigit() }.digitToInt()
+            return@sumOf firstNumber * 10 + lastNumber
+        }
     }
+
+    val digitWords = listOf(
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        "1", "2", "3", "4", "5", "6", "7", "8", "9"
+    )
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val firstDigit = line.findAnyOf(digitWords)!!
+            val lastDigit = line.findLastAnyOf(digitWords)!!
+
+            val firstNumber = if (firstDigit.second.length == 1) {
+                firstDigit.second.toInt()
+            } else {
+                digitWords.indexOf(firstDigit.second) + 1
+            }
+            val lastNumber = if (lastDigit.second.length == 1) {
+                lastDigit.second.toInt()
+            } else {
+                digitWords.indexOf(lastDigit.second) + 1
+            }
+
+            return@sumOf firstNumber * 10 + lastNumber
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
